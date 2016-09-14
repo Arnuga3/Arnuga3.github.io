4b27c252af272ca166d5a9c79bbaf45d0c67d5a9
Arnuga3 <20136310@nescol.ac.uk>
2016-09-14 15:16:10 +0100

    jump and gravity added

--- canvas_game.js
+++ canvas_game.js
@@ -29,6 +29,14 @@ init();
 var hero = new Character(cellSize);
 
 
+window.onclick = function() {
+  hero.jump();
+};
+window.touchstart = function() {
+  hero.jump();
+};
+
+
 // resize a canvas
 function resizeCanvas() {
   canvas.width = window.innerWidth;
@@ -52,25 +60,60 @@ function run() {
 	time = Date.now();
 }
 
-function update(mod) {
-
-}
 
 // counter to change run images
 var runCounter = 0;
 
+function update(mod) {
+
+  if (!hero.jumping) {
+
+    // if on ground - running
+    if (hero.landing == false) {
+      runCounter += 1;
+      if (runCounter > 7) {
+        hero.runPosition = hero.runPosition == 0 ? 1 : 0;
+        runCounter = 0;
+      }
+    }
+
+    hero.posY += hero.speed * mod;
+    if (hero.posY + cellSize * 20 >= canvas.height) {
+      hero.posY -= hero.speed * mod;
+      hero.landing = false;
+    }
+
+  } else {
+    // in air
+    hero.landing = true;
+    // reset counter for later use
+    runCounter = 0;
+    // draw a jumping image
+    hero.runPosition = 2;
+
+    // move up
+    hero.posY -= hero.speed * mod;
+		hero.jumpHeightCounter -= 1;
+
+    // reached jump height
+    if (hero.jumpHeightCounter <= 0) {
+      hero.jumping = false;
+      hero.jumpHeightCounter = hero.jumpHeight;
+
+    }
+  }
+
+}
+
 function drawScene() {
 
-  runCounter += 1;
-  if (runCounter > 10) {
-    hero.runPosition = hero.runPosition == 0 ? 1 : 0;
-    runCounter = 0;
-  }
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   if (hero.runPosition == 0) {
-    ctx.drawImage(hero.image_1, hero.posX, hero.posY);
-  } else {
     ctx.drawImage(hero.image_2, hero.posX, hero.posY);
+  } else if(hero.runPosition == 1) {
+    ctx.drawImage(hero.image_1, hero.posX, hero.posY);
+  } else if(hero.runPosition == 2) {
+    ctx.drawImage(hero.image_3, hero.posX, hero.posY);
   }
 }
 
@@ -81,10 +124,16 @@ function Character(cellSize) {
 
   this.cellSize = cellSize;
 
+  // image type pointer
+  this.runPosition = 0;
+  this.jumpHeight = 30;
+  this.jumpHeightCounter = this.jumpHeight;
+
   this.posX = this.cellSize*10;
   this.posY = this.cellSize*40;
-  this.speed = 100;
-  this.runPosition = 0;
+  this.speed = this.cellSize*60;
+  this.jumping = false;
+  this.landing = false;
 
   this.image_1;
   this.image_2;
@@ -133,14 +182,14 @@ function Character(cellSize) {
               [' ',' ',' ','#','#',' ',' ',' ',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' '],
               [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '] ],
 
-              [ [' ',' ',' ',' ','#','#',' ',' ',' ',' ',' ',' ','#','#',' ',' ',' ',' ',' '],
+              [ [' ',' ',' ',' ','#','#',' ',' ',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' '],
                 [' ',' ',' ','#',' ',' ','#',' ',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' '],
                 [' ',' ',' ',' ',' ',' ','#','#','#','#','#','#','#','#','#',' ',' ',' ',' '],
                 [' ',' ',' ',' ',' ','#','#','#',' ',' ',' ',' ',' ',' ','#','#','#',' ',' '],
                 [' ',' ',' ',' ','#','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#',' ',' '],
-                ['#','#','#','#','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#','#',' '],
-                ['#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#',' ','#',' ',' ',' ','#',' '],
-                ['#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#',' ','#',' ',' ',' ','#',' '],
+                [' ','#','#','#','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#','#',' '],
+                ['#','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#',' '],
+                ['#',' ',' ',' ',' ',' ',' ',' ',' ',' ','#','#',' ','#','#',' ',' ','#',' '],
                 ['#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#','#'],
                 ['#','#',' ',' ',' ',' ',' ',' ',' ','#',' ','#',' ','#',' ',' ',' ',' ','#'],
                 [' ','#',' ',' ',' ',' ',' ',' ','#','#','#','#','#','#','#',' ',' ',' ','#'],
@@ -148,8 +197,8 @@ function Character(cellSize) {
                 [' ','#','#',' ',' ',' ',' ','#','#','#','#','#','#','#','#',' ',' ',' ','#'],
                 [' ',' ','#',' ',' ',' ',' ',' ','#','#','#','#','#','#','#',' ',' ','#',' '],
                 [' ',' ','#','#',' ',' ',' ',' ',' ','#',' ','#',' ','#',' ',' ',' ','#',' '],
-                [' ',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#',' ',' '],
-                [' ',' ',' ','#','#','#','#',' ',' ',' ',' ',' ',' ','#','#','#',' ',' ',' '],
+                [' ',' ',' ','#','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#',' ',' '],
+                [' ',' ',' ',' ','#','#','#',' ',' ',' ',' ',' ',' ','#','#','#',' ',' ',' '],
                 [' ',' ',' ',' ',' ',' ','#','#','#','#','#','#','#','#',' ',' ',' ',' ',' '],
                 [' ',' ',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' '],
                 [' ',' ',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' '] ]
@@ -159,6 +208,7 @@ function Character(cellSize) {
     this.cellSize = cellSize;
     this.posX = this.cellSize*10;
     this.posY = this.cellSize*40;
+    this.speed = this.cellSize*60;
     this.drawImages();
   };
 
@@ -203,6 +253,10 @@ function Character(cellSize) {
     this.image_1 = this.createImage(0);
     this.image_2 = this.createImage(1);
     this.image_3 = this.createImage(2);
+  };
+
+  this.jump = function() {
+    this.jumping = true;
   };
 
 }
