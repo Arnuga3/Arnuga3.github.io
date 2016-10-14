8251ba55f9781f403751ad72bf27c56b1b879f22
T.Chmelevskij <T.Chmelevskij@gmail.com>
2016-10-14 16:24:53 +0100

    Refactor
    - Change the definition of objects. Rather then extending
      Object.prototype it should be replaced with own one. Reason for that
      is because of prototype chain newly defined object will inherit it
      from global Object anyway.
    
    - Use js syntax for getters and setters.
    
    - Use window.requestAnimationFrame and window.cancelAnimationFrame for
      animations to keep them at ~60ps.
    
    - Separate files just for clarity.

--- canvas_game.js
+++ canvas_game.js
@@ -1,414 +1,125 @@
-/// PIXEL ART ///
-////////////////////////////////////////////////////////////////////////////////
-
-pixCharacter =  [
-  [ [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#','#',' ',' ',' ',' ',' ',' '],
-    [' ',' ',' ',' ','#','#','#',' ',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' '],
-    [' ',' ','#','#',' ',' ','#',' ',' ',' ',' ',' ',' ','#','#',' ',' ',' ',' '],
-    [' ',' ',' ',' ',' ',' ','#','#','#','#','#','#','#','#','#','#','#',' ',' '],
-    [' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#',' ',' '],
-    [' ',' ',' ',' ','#','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#','#',' '],
-    ['#','#','#','#','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#',' '],
-    ['#',' ',' ',' ',' ',' ',' ',' ',' ',' ','#','#',' ','#','#',' ',' ','#',' '],
-    ['#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#','#'],
-    ['#','#',' ',' ',' ',' ',' ',' ',' ','#',' ','#',' ','#',' ',' ',' ',' ','#'],
-    [' ','#',' ',' ',' ',' ',' ',' ','#','#','#','#','#','#','#',' ',' ',' ','#'],
-    [' ','#',' ',' ',' ',' ',' ','#','#','#','#','#','#','#','#','#',' ',' ','#'],
-    [' ','#','#',' ',' ',' ',' ','#','#','#','#','#','#','#','#','#',' ',' ','#'],
-    [' ',' ','#',' ',' ',' ',' ',' ','#','#','#','#','#','#','#','#',' ','#',' '],
-    [' ',' ','#','#',' ',' ',' ',' ',' ','#',' ','#',' ','#',' ',' ',' ','#',' '],
-    [' ',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#',' ',' '],
-    [' ',' ',' ','#','#','#','#',' ',' ',' ',' ',' ',' ','#','#','#',' ',' ',' '],
-    [' ',' ',' ',' ',' ','#','#','#','#','#','#','#','#','#',' ','#',' ',' ',' '],
-    [' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ','#',' ',' ',' '],
-    [' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ','#',' ',' ',' '] ],
-
-  [ [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#','#','#',' ',' ',' ',' ',' '],
-    [' ',' ','#','#','#','#','#',' ',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' '],
-    [' ',' ',' ',' ',' ',' ','#','#','#','#','#','#','#','#','#',' ',' ',' ',' '],
-    [' ',' ',' ',' ',' ','#','#','#',' ',' ',' ',' ',' ',' ','#','#','#',' ',' '],
-    [' ',' ',' ',' ','#','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#',' ',' '],
-    ['#','#','#','#','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#','#',' '],
-    ['#',' ',' ',' ',' ',' ',' ',' ',' ',' ','#','#',' ','#','#',' ',' ','#',' '],
-    ['#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#',' '],
-    ['#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#',' '],
-    ['#','#',' ',' ',' ',' ',' ',' ',' ','#',' ','#',' ','#',' ',' ',' ','#',' '],
-    [' ','#',' ',' ',' ',' ',' ',' ','#','#','#','#','#','#','#',' ',' ','#',' '],
-    [' ','#',' ',' ',' ',' ',' ','#','#','#','#','#','#','#','#',' ',' ','#',' '],
-    [' ','#','#',' ',' ',' ','#','#','#','#','#','#','#','#','#',' ',' ','#',' '],
-    [' ',' ','#',' ',' ',' ','#','#','#','#','#','#','#','#','#',' ',' ','#',' '],
-    [' ',' ','#','#',' ',' ',' ','#',' ','#',' ','#',' ','#',' ',' ',' ','#',' '],
-    [' ',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#',' ',' '],
-    [' ',' ',' ','#','#','#','#',' ',' ',' ',' ',' ',' ','#','#','#',' ',' ',' '],
-    [' ',' ',' ',' ','#',' ','#','#','#','#','#','#','#','#',' ',' ',' ',' ',' '],
-    [' ',' ',' ','#','#',' ',' ',' ',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' '],
-    [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '] ],
-
-    [ [' ',' ',' ',' ','#','#',' ',' ',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' '],
-      [' ',' ',' ','#',' ',' ','#',' ',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' '],
-      [' ',' ',' ',' ',' ',' ','#','#','#','#','#','#','#','#','#',' ',' ',' ',' '],
-      [' ',' ',' ',' ',' ','#','#','#',' ',' ',' ',' ',' ',' ','#','#','#',' ',' '],
-      [' ',' ',' ',' ','#','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#',' ',' '],
-      [' ',' ',' ','#','#',' ',' ',' ',' ',' ',' ','#',' ','#',' ',' ','#','#',' '],
-      [' ',' ','#','#',' ',' ',' ',' ',' ',' ',' ','#',' ','#',' ',' ',' ','#',' '],
-      [' ','#','#',' ',' ',' ',' ',' ',' ',' ',' ','#',' ','#',' ',' ',' ','#',' '],
-      [' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#','#'],
-      ['#','#',' ',' ',' ',' ',' ',' ',' ','#','#','#','#','#',' ',' ',' ',' ','#'],
-      ['#',' ',' ',' ',' ',' ',' ',' ','#','#','#','#','#','#','#',' ',' ',' ','#'],
-      ['#','#',' ',' ',' ',' ',' ',' ','#','#','#','#','#','#','#',' ',' ',' ','#'],
-      [' ','#',' ',' ',' ',' ',' ',' ','#','#','#','#','#','#','#',' ',' ',' ','#'],
-      [' ','#','#',' ',' ',' ',' ',' ',' ','#','#','#','#','#',' ',' ',' ','#',' '],
-      [' ',' ','#','#',' ',' ',' ',' ',' ',' ','#','#','#',' ',' ',' ',' ','#',' '],
-      [' ',' ',' ','#','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#',' ',' '],
-      [' ',' ',' ',' ','#','#','#',' ',' ',' ',' ',' ',' ','#','#','#',' ',' ',' '],
-      [' ',' ',' ',' ',' ',' ','#','#','#','#','#','#','#','#',' ',' ',' ',' ',' '],
-      [' ',' ',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' '],
-      [' ',' ',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' '] ],
-
-      [ [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
-        [' ',' ',' ',' ','#','#','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
-        [' ',' ',' ',' ','#',' ','#','#','#','#','#','#','#','#','#',' ','#',' ',' '],
-        [' ',' ',' ',' ',' ','#','#','#',' ',' ',' ',' ',' ',' ','#','#',' ','#','#'],
-        [' ',' ',' ',' ','#','#',' ',' ',' ',' ','#','#',' ',' ',' ',' ','#',' ',' '],
-        [' ',' ','#','#','#',' ',' ',' ',' ','#',' ',' ','#',' ','#','#','#','#',' '],
-        [' ',' ','#',' ',' ',' ',' ',' ','#','#',' ','#','#','#','#',' ','#','#',' '],
-        [' ','#','#',' ',' ',' ',' ',' ','#','#',' ',' ','#','#',' ',' ','#','#',' '],
-        [' ','#',' ',' ',' ',' ',' ',' ',' ','#','#','#',' ','#','#','#','#','#','#'],
-        [' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#','#',' ',' ','#'],
-        [' ','#',' ',' ',' ',' ',' ',' ','#','#','#','#','#',' ',' ',' ',' ',' ','#'],
-        [' ','#',' ',' ',' ',' ',' ','#','#','#','#','#','#','#',' ',' ',' ',' ','#'],
-        [' ','#','#',' ',' ',' ',' ','#','#',' ','#',' ','#','#','#',' ',' ',' ','#'],
-        [' ',' ','#',' ',' ',' ',' ','#','#',' ',' ',' ','#','#','#',' ',' ','#',' '],
-        [' ',' ','#','#',' ',' ',' ',' ','#',' ',' ',' ','#',' ',' ',' ',' ','#',' '],
-        [' ',' ',' ','#','#',' ',' ',' ',' ','#','#','#',' ',' ',' ',' ','#',' ',' '],
-        [' ',' ',' ',' ','#','#','#',' ',' ',' ',' ',' ',' ','#','#','#',' ',' ',' '],
-        [' ',' ',' ',' ',' ',' ','#','#','#','#','#','#','#','#',' ',' ',' ',' ',' '],
-        [' ',' ',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' '],
-        [' ',' ',' ',' ',' ','#','#',' ',' ',' ',' ',' ',' ',' ','#',' ',' ',' ',' '] ]
-];
-
-
-pixBarrierHTML = [[
-    ['#',' ','#','#','#','#','#',' ',' ',' ','#',' '],
-    ['#',' ','#',' ','#',' ','#','#',' ','#','#',' '],
-    ['#','#','#',' ','#',' ','#',' ','#',' ','#',' '],
-    ['#',' ','#',' ','#',' ','#',' ',' ',' ','#',' '],
-    ['#',' ','#',' ','#',' ','#',' ',' ',' ','#','#']]
-];
-
-
-/// CLASSES ///
-////////////////////////////////////////////////////////////////////////////////
-/// BLOCK CLASS (Constructor) ///
-
-function Block(cellSize, x, y, speed, pix) {
-  this.cellSize = cellSize;
-  this.posX = x * cellSize;
-  this.posY = y * cellSize;
-  this.pix = pix;
-  this.speed = speed * cellSize;
-  this.images = [];
-}
-
-Block.prototype.getX = function() {
-  return this.posX;
-}
-
-Block.prototype.getY = function() {
-  return this.posY;
-}
-
-Block.prototype.getSpeed = function() {
-  return this.speed;
-}
-
-Block.prototype.setX = function(x) {
-  this.posX = x;
-}
-
-Block.prototype.setY = function(y) {
-  this.posY = y;
-}
-
-Block.prototype.setSpeed = function(speed) {
-  this.speed = speed;
-}
-
-Block.prototype.drawFromArray = function(array, canvasCtx) {
-  //start coordinates on the canvas, left-top corner
-  var coord_x = 0;
-  var coord_y = 0;
-  var arrLength = array.length;
-
-  for (var i=0; i<arrLength; i++){		//start from first line of the map array
-    var rowLength = array[i].length;
-
-    for (var j=0; j<rowLength; j++){		//check each element in that line
-
-      if (array[i][j] === '#'){
-        canvasCtx.fillStyle = pixColor;
-        canvasCtx.fillRect(coord_x, coord_y, this.cellSize, this.cellSize);
-      }
-      coord_x += this.cellSize;
-    }
-    coord_y += this.cellSize;		//after line check is complete, it goes to next map line
-    coord_x = 0;		//sets x to 0, as check of new line start from the beginning
-  }
-}
-
-Block.prototype.createImage = function(imageNr) {
-    // create a character canvas
-    var picture = document.createElement('canvas');
-
-    // 20 cells width and height
-    picture.width = this.cellSize*20;
-    picture.height = this.cellSize*20;
-    var pictureCtx = picture.getContext('2d');
-
-    // draw a character image
-    this.drawFromArray(this.pix[imageNr], pictureCtx);
-    return picture;
-}
-
-Block.prototype.drawImages = function() {
-  //draw character images
-  this.images = [];
-  for (var i=0; i<this.pix.length; i++) {
-    var image = this.createImage(i);
-    this.images.push(image);
-  }
-}
-
-Block.prototype.getImage = function(num) {
-  return this.images[num];
-}
-
-Block.prototype.fitSize = function(cellSize, reset) {
-  this.cellSize = cellSize;
-  this.setX(reset.x);
-  this.setY(reset.y * cellSize);
-  this.setSpeed(reset.speed * cellSize);
-  this.drawImages();
-  this.width = Math.round(this.pix[0][0].length * cellSize);
-  this.height = Math.round(this.pix[0].length * cellSize);
-};
-
-
-/// CHARACTER CLASS (Constructor) ///
-
-function Character(cellSize, x, y, speed, pix) {
-  // Reuse initialization
-  Block.call(this, cellSize, x, y, speed, pix);
-
-  // image type pointer
-  this.runPosition = 0;
-  this.jumpHeight = 25;
-  this.jumpHeightCounter = this.jumpHeight;
-  this.jumping = false;
-  this.landing = false;
-}
-
-// inherit form a Block
-Character.prototype = new Block();
-// use its own constructor
-Character.prototype.constructor = Character;
-// add a method
-Character.prototype.jump = function() {
-  this.jumping = true;
-};
-
-
-
-/// BARRIER CLASS (Constructor) ///
-
-function Barrier(cellSize, x, y, speed, pix) {
-  // Reuse initialization
-  Block.call(this, cellSize, x, y, speed, pix);
-
-}
-
-// inherit form a Block
-Barrier.prototype = new Block();
-// use its own constructor
-Barrier.prototype.constructor = Barrier;
-
-
-/// GAME LOGIC ///
-////////////////////////////////////////////////////////////////////////////////
 var canvas;
 var ctx;
 var cellSize;
 var game;
+var hero;
 var pixColor = 'black';
-
+var animation;
 canvas = document.getElementById('canvas-game');
 ctx = canvas.getContext("2d");
 
-
-
-/// EVENTS ///
-
-// on resize or screen orientation change
 window.addEventListener('resize', resizeCanvas, false);
 window.addEventListener('orientationchange', resizeCanvas, false);
-
 window.onclick = function() {
-  if (hero.landing == false) {
-    hero.jump();
-  }
-};
-
+    if (hero.inAir == false) {
+        hero.jump();
+    }
+}
+;
 window.touchstart = function() {
-  if (hero.landing == false) {
-    hero.jump();
-  }
-};
-
-
-
+    if (hero.inAir == false) {
+        hero.jump();
+    }
+}
+;
 /// COLLISION FUNCTION ///
-
 function collides(a, b) {
-  return a.posX < b.posX + b.width &&
-		 a.posX + a.width > b.posX &&
-		 a.posY < b.posY + b.height &&
-		 a.posY + a.height > b.posY;
+    return (
+            a.x < b.x + b.width &&
+            a.x + a.width > b.x &&
+            a.y < b.y + b.height &&
+            a.y + a.height > b.y
+    );
 }
-
-
-
 // OBJECTS DEFINITION ///
-
-// reset values (no effect from a canvas size)
 var heroReset = {
-  x: canvas.width/12,
-  y: 40,
-  speed: 80
+    x: canvas.width / 12,
+    y: 40,
+    speed: 80
 };
-var hero = new Character(cellSize, heroReset.x, heroReset.y, heroReset.speed, pixCharacter);
-
-// reset values (no effect from a canvas size)
+hero = new Character(cellSize,heroReset.x,heroReset.y,heroReset.speed,pixCharacter);
 var barrierReset = {
-  x: 0,
-  y: 55,
-  speed: 60
+    x: 0,
+    y: 55,
+    speed: 60
 };
-var barrier1 = new Barrier(cellSize, barrierReset.x, barrierReset.y, barrierReset.speed, pixBarrierHTML);
+var barrier1 = new Barrier(cellSize,barrierReset.x,barrierReset.y,barrierReset.speed,pixBarrierHTML);
 
-
-
-// draw a first canvas
 function init() {
-  canvas.width = window.innerWidth;
-  canvas.height = canvas.width/4;
-
-  var seizePoint = canvas.height/3;
-
-  // height and width of an image cell
-  cellSize = seizePoint/20;
+    canvas.width = window.innerWidth;
+    canvas.height = canvas.width / 4;
+    var seizePoint = canvas.height / 3;
+    cellSize = seizePoint / 20;
 }
+
 init();
 
-
-// resize a canvas
 function resizeCanvas() {
-  canvas.width = window.innerWidth;
-  canvas.height = canvas.width/4;
-
-  var seizePoint = canvas.height/3;
-
-  // height and width of an image cell
-  cellSize = seizePoint/20;
-
-  // reset x position on canas width change
-  barrierReset.x = canvas.width;
-  heroReset.x = canvas.width/12;
-
-  // resize character images and fit positions
-  hero.fitSize(cellSize, heroReset);
-  barrier1.fitSize(cellSize, barrierReset);
+    canvas.width = window.innerWidth;
+    canvas.height = canvas.width / 4;
+    var seizePoint = canvas.height / 3;
+    cellSize = seizePoint / 20;
+    barrierReset.x = canvas.width;
+    heroReset.x = canvas.width / 12;
+    hero.fitSize(cellSize, heroReset);
+    barrier1.fitSize(cellSize, barrierReset);
 }
+
 resizeCanvas();
 
-
-// loop function
 function run() {
-	update((Date.now() - time) / 1000);
-	drawScene();
-	time = Date.now();
+    animation = requestAnimationFrame(run);
+    update(0.02);
+    drawScene();
 }
 
-
-// counter to change run images
 var runCounter = 0;
-
 function update(mod) {
-
-  /// HERO ///
-  if (!hero.jumping) {
-
-    // if on ground, running
-    if (hero.landing == false) {
-      runCounter += 1;
-      if (runCounter > 7) {
-        hero.runPosition = hero.runPosition == 0 ? 1 : 0;
+    if (!hero.jumping) {
+        if (hero.inAir == false) {
+            runCounter += 1;
+            if (runCounter > 7) {
+                hero.runPosition = hero.runPosition == 0 ? 1 : 0;
+                runCounter = 0;
+            }
+        }
+        hero.y = hero.y + hero.speed * mod;
+        if (hero.y + cellSize * 20 > canvas.height) {
+            hero.y = hero.y - hero.speed * mod;
+            hero.inAir = false;
+        }
+    } else {
+        hero.inAir = true;
         runCounter = 0;
-      }
+        hero.runPosition = 2;
+        hero.y = hero.y - hero.speed * mod;
+        hero.jumpHeightCounter -= 1;
+        if (hero.jumpHeightCounter <= 0) {
+            hero.jumping = false;
+            hero.jumpHeightCounter = hero.jumpHeight;
+        }
     }
 
-    // gravity, if statement do not let to fall under the ground
-    // same as hero.posY += hero.speed * mod
-    hero.setY(hero.getY() + hero.getSpeed() * mod);
-    if (hero.getY() + cellSize * 20 > canvas.height) {
-      hero.setY(hero.getY() - hero.getSpeed() * mod);
-      hero.landing = false;
+    /// BARRIERS ///
+    barrier1.x = barrier1.x - barrier1.speed * mod;
+    if (barrier1.x <= -100) {
+        barrier1.x = barrierReset.x;
     }
 
-  } else {
-    // in air
-    hero.landing = true;
-    // reset counter for later use
-    runCounter = 0;
-    // draw a jumping image
-    hero.runPosition = 2;
-
-    // move up, jumping
-    hero.setY(hero.getY() - hero.getSpeed() * mod);
-		hero.jumpHeightCounter -= 1;
-
-    // reached jump height
-    if (hero.jumpHeightCounter <= 0) {
-      hero.jumping = false;
-      hero.jumpHeightCounter = hero.jumpHeight;
+    /// COLLISIONS ///
+    if (collides(hero, barrier1)) {
+        window.cancelAnimationFrame(animation);
+        hero.runPosition = 3;
     }
-  }
-
-  /// BARRIERS ///
-  barrier1.setX(barrier1.getX() - barrier1.getSpeed() * mod);
-  if (barrier1.getX() <= -100) {
-    barrier1.setX(barrierReset.x);
-  }
-
-  /// COLLISIONS ///
-  if (collides(hero, barrier1)) {
-    console.log("collision");
-    clearInterval(gameLoop);
-    hero.runPosition = 3;
-  }
 }
 
 function drawScene() {
-
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  // draw a character image depending on the runPosition
-  var currentImage = function(obj, nr) {
-    ctx.drawImage(obj.getImage(nr), obj.getX(), obj.getY());
-  };
-  currentImage(hero, hero.runPosition);
-  currentImage(barrier1, 0);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    var currentImage = function(obj, nr) {
+        ctx.drawImage(obj.getImage(nr), obj.x, obj.y);
+    };
+    currentImage(hero, hero.runPosition);
+    currentImage(barrier1, 0);
 }
 
-
-var time = Date.now();
-var gameLoop = setInterval(run, 20);
+animation = requestAnimationFrame(run);
